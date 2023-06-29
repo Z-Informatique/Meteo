56367888f30cd608d99056ee434ffca3f4446e0a
TSIAZE KENFACK <tsiazezephirin@gmail.com>
2023-06-29 12:06:33 +0100

    Création des différentes pages, modification sdes fichiers de ressources et des fichiers App et AppShell

--- Meteo/AppShell.xaml.cs
+++ Meteo/AppShell.xaml.cs
@@ -1,9 +1,28 @@
-﻿namespace Meteo;
+﻿using System.Diagnostics;
+
+namespace Meteo;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+
+        if (DeviceInfo.Idiom == DeviceIdiom.Phone)
+        {
+            Shell.Current.CurrentItem = PhoneTabs;
+        }
+    }
+
+    private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+		try
+		{
+			await Shell.Current.GoToAsync($"///settings");
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine($"err: {ex.Message}");
+		}
+    }
 }
---
+++ Meteo/Pages/MapPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Meteo.Pages;
+
+public partial class MapPage : ContentPage
+{
+	public MapPage()
+	{
+		InitializeComponent();
+	}
+}
