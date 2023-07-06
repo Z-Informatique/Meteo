51cfce7785bc718906e56b2b6153fbacce6b80a5
TSIAZE KENFACK <tsiazezephirin@gmail.com>
2023-07-06 11:33:55 +0100

    Finalisation de la page Favoris et de la page MapPage

--- Meteo/Pages/FavoritesPage.xaml.cs
+++ Meteo/Pages/FavoritesPage.xaml.cs
@@ -1,3 +1,6 @@
+using Android.Transitions;
+using Meteo.ViewModels;
+
 namespace Meteo.Pages;
 
 public partial class FavoritesPage : ContentPage
@@ -5,5 +8,31 @@ public partial class FavoritesPage : ContentPage
 	public FavoritesPage()
 	{
 		InitializeComponent();
+		BindingContext = new FavoritesViewModel();
+	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+
+		await Task.Delay(300);
+        TransitionIn();
+	}
+	int tileCount = 0;
+	List<Frame> tiles = new List<Frame>();
+    private async void TransitionIn()
+    {
+		foreach (var item in tiles)
+		{
+			await item.FadeTo(1, 800);
+			await Task.Delay(50);
+		}
+    }
+
+	void OnHandlerChanged(object sender, EventArgs e)
+	{
+		Frame f = (Frame)sender;
+		tiles.Add(f);
+		tileCount++;
 	}
 }
